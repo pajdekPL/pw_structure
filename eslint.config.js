78bb3fd437c4e1e410252321c4296c972d2f14d2
Adam Pajda <adam_pajda@outlook.com>
2024-11-30 11:04:42 +0100

    chore: update after cr

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
-import { default as eslint } from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import playwright from "eslint-plugin-playwright";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
